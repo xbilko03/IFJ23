1b22eeb18fca2b9049e277b6ffc5017b2ea15c15
Spini11 <viliamchudacik@gmail.com>
2023-11-12 12:25:52 +0100

    Lexer reworked into state machine

---
+++ lex.h
@@ -0,0 +1,64 @@
+#ifndef LEX_H
+#define LEX_H
+#include <ctype.h> 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include "functions.h"
+
+typedef enum MachineStates
+{
+    Start,
+    String_start,
+    String_end,
+    String_escape,
+    Operator3,
+    Comment_one_line,
+    Comment_multi_line_start,
+    Comment_multi_line_end1,
+    Comment_multi_line_end2,
+    Compare_start,
+    Compare_end,
+    Open_bracket,
+    Close_bracket,
+    Open_block,
+    Close_block,
+    Colon,
+    Comma,
+    Operator,
+    Operator4,
+    Arrow,
+    Assign,
+    Compare,
+    ExclamationMark,
+    Compare2,
+    Identifier,
+    Identifier_type,
+    Identifier_start,
+    Integer,
+    Double_start,
+    Double,
+    Double_exp,
+    Double_exp_op,
+    Double_exp_op2,
+    Newline,
+    Error,
+    Endoffile,
+    End,
+} MachineStates;
+
+
+
+MachineStates StateMachine (char input, MachineStates currentState);
+void Tokenizer(wordStr *wordList);
+void AddToken(MachineStates state, wordStr **wordList, char* word);
+char* EscapeSequence(char* word);
+char HexEscape(char* word, int *i, int *lenght);
+bool Keyword(char* word);
+bool Type(char* word);
+void PrintWordList(wordStr *wordList);
+
+#endif
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "functions.h"
 #include <stdlib.h>
+#include "lex.h"
 
 int main(int argc, char **argv)
 {
@@ -9,11 +10,17 @@ int main(int argc, char **argv)
 	//Create Word List
 	wordListStr* wordList;
 	wordList = malloc(sizeof(wordListStr));
-	wordList->first = NULL;
-	wordList->last = NULL;
+	wordList->first = malloc(sizeof(wordStr));
+	wordList->last = wordList->first;
+	wordList->first->type = NULL;
+	wordStr* LastToken = wordList->first;
 	
 	//Fill Word List and Check for Lex Rules
-	PerformLex(wordList);
+	Tokenizer(LastToken);
+
+	PrintWordList(wordList->first);
+
+	printf("--------------------------------\n\n");
 
 	PerformSyntax(wordList);
 
@@ -21,7 +28,7 @@ int main(int argc, char **argv)
 
 	PerformCodeGen();
 
-	DeleteWordList(wordList);
+	//DeleteWordList(wordList);
 
 	printf("\nEnd\n");
 	return 0;
