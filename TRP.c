0032f08267c2e157cec0409c10d5e2c8dc53e6d2
Ardival <105982214+Ardival@users.noreply.github.com>
2023-11-19 21:32:28 +0100

    funkcie TRP

--- TRP.c
+++ TRP.c
@@ -3,19 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-void TableAddItem(TRP* table, wordStr* item)
-{
-
-}
-wordStr* TableFindItem()
-{
-
-return NULL;
-}
-void TableRemoveItem(TRP* table, wordStr* item)
-{
-
-}
 long HashFunction(char* key)
 {
 	long result = 0;
@@ -27,4 +14,73 @@ long HashFunction(char* key)
 	}
 
 	return result % hashTableSize;
+}
+
+void TableInit(TRP* table){
+	for (int i = 0; i < hashTableSize; i++){
+    	table->items[i] = NULL;
+  	}
+}
+
+TRPitem* TableFindItem(TRP* table, char *key)
+{
+	int hash = HashFunction(key);
+	TRPitem* current = table->items[hash];
+
+	while (current != NULL) {
+		if (current->type == key){
+		return current;
+		}
+		current = current->next;
+	}
+	return NULL;
+}
+
+void TableAddItem(TRP* table, char* key, wordStr* type, char* content)
+{
+	int hash = get_hash(key);
+  	TRPitem *item = ht_search(table, key);
+
+	if (item != NULL){
+		item->type = type;
+		return;
+	} else {
+	
+		TRPitem *new_item = malloc(sizeof(TRPitem));
+		if (new_item == NULL){return;}
+
+		new_item->content = content;
+		new_item->key = key; 
+		new_item->type = type;
+		new_item->next = NULL;
+
+    if (table->items[hash] == NULL){ 
+      table->items[hash] = new_item;
+      return;
+    } else {
+      TRPitem *last_item = table->items[hash];
+
+      while (last_item != NULL){ 
+        if (last_item->next != NULL){
+          last_item = last_item->next;
+        } else {
+          last_item->next = new_item;
+          return;
+        }
+      }
+    }
+  }
+}
+void TableRemoveTable(TRP* table, wordStr* item)
+{
+	for (int i = 0; i < hashTableSize; i++){
+		TRPitem *current = table->items[i];
+		
+		while (current != NULL){
+		TRPitem *next = current->next;
+		free(current);
+		current = next;
+		}
+		table->items[i] = NULL;
+	}
 }
