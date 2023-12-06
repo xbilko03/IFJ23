dcdbd8d32d14eb4d9c5b37c973639d12f4959417
jozef <jozef@B07-322C>
2023-12-06 19:39:25 +0100

    Functions bug fixes + basic inbuilt functions

--- codegen.c
+++ codegen.c
@@ -23,7 +23,6 @@ typedef struct
 symStr symTables[256];
 unsigned int symIndex = 0;
 unsigned int symID = 0;
-unsigned int paramIndex = 0;
 int functionActiveFlag = 0;
 
 unsigned int FindTableIndex(char* symbol)
@@ -39,6 +38,15 @@ void ADD(Node* c_symb);
 void SUB(Node* c_symb);
 void IDIV(Node* c_symb);
 void MUL(Node* c_symb);
+void INBUILT_LENGTH(Node* c_node);
+void INBUILT_INT2DOUBLE(Node* c_node);
+void INBUILT_READSTRING(Node* c_node);
+void INBUILT_READINT(Node* c_node);
+void INBUILT_WRITE(Node* c_node);
+void INBUILT_DOUBLE2INT(Node* c_node);
+void INBUILT_SUBSTRING(Node* c_node);
+void INBUILT_ORD(Node* c_node);
+void INBUILT_CHR(Node* c_node);
 
 /* Print to stdout */
 void PrintCode(char* code)
@@ -133,6 +141,11 @@ void PrintSymbol(Node* c_symb)
 		PrintCode("GF@");
 		PrintCode(c_symb->content);
 		PrintCode("_func");
+	}
+	else
+	{
+		if (strcmp(c_symb->type, "function") == 0)
+			PrintCode("INT@0");
 	}
 
 	return;
@@ -458,8 +471,24 @@ void ProcessNode(Node* c_node)
 	}
 	else if (strcmp(c_node->type, "function") == 0)
 	{
-		if(strcmp(c_node->content, "write") == 0)
+		if (strcmp(c_node->content, "write") == 0)
 			INBUILT_WRITE(c_node);
+		else if (strcmp(c_node->content, "Int2Double") == 0)
+			INBUILT_INT2DOUBLE(c_node);
+		else if (strcmp(c_node->content, "Double2Int") == 0)
+			INBUILT_DOUBLE2INT(c_node);
+		else if (strcmp(c_node->content, "readString") == 0)
+			INBUILT_READSTRING(c_node);
+		else if (strcmp(c_node->content, "readInt") == 0)
+			INBUILT_READINT(c_node);
+		else if (strcmp(c_node->content, "length") == 0)
+			INBUILT_LENGTH(c_node);
+		else if (strcmp(c_node->content, "substring") == 0)
+			INBUILT_SUBSTRING(c_node);
+		else if (strcmp(c_node->content, "ord") == 0)
+			INBUILT_ORD(c_node);
+		else if (strcmp(c_node->content, "chr") == 0)
+			INBUILT_CHR(c_node);
 	}
 	else if (strcmp(c_node->type, "assign") == 0)
 	{
@@ -483,7 +512,10 @@ void ProcessNode(Node* c_node)
 				PrintCode(c_node->children[i]->content);
 				PrintCode("_func");
 				PrintCode(" ");
-				PrintSymbol(c_node->children[i]->children[0]);
+				if(c_node->children[i]->numChildren > 0)
+					PrintSymbol(c_node->children[i]->children[0]);
+				else
+					PrintSymbol(c_node->children[i]);
 				PrintCode("\n");
 			}
 
@@ -559,42 +591,6 @@ void MOVE(Node* c_var, Node* c_symb)
 }
 
 /*
-* CREATEFRAME
-* Vytvor novy docasny ramec
-* Vytvori novy docasny ramec a zahodi pripadny obsah puvodniho docasneho ramce.
-*/
-void CREATEFRAME()
-{
-	PrintCode("CREATEFRAME\n");
-	return;
-}
-
-/*
-* PUSHFRAME
-* Presun docasneho ramce na zasobnik ramcu
-* Presun TF na zasobnik ramcu.Ramec bude k dispozici pres LF a prekryje puvodni
-* ramce na zasobniku ramcu.TF bude po provedeni instrukce nedefinovan a je treba jej
-* pred dalsim pouzitim vytvorit pomoci CREATEFRAME. Pokus o pristup k nedefinovanemu ramci vede na chybu 55.
-*/
-void PUSHFRAME()
-{
-	PrintCode("PUSHFRAME\n");
-	return;
-}
-
-/*
-* POPFRAME
-* P?esun aktu�ln�ho r�mce do do?asn�ho
-* Presun vrcholovy ramec LF ze zasobniku ramcu do TF. 
-* Pokud zadny ramec v LF neni k dispozici, dojde k chybe 55.
-*/
-void POPFRAME()
-{
-	PrintCode("POPFRAME\n");
-	return;
-}
-
-/*
 * DEFVAR ?var?
 * Definuj novou prom?nnou v ramci
 * Definuje promennou v urcenem ramci dle ?var?. Tato promenn� je zatim neinicializovana 
@@ -637,60 +633,6 @@ void DEFVAR(Node* c_var)
 	return;
 }
 
-/*
-* CALL ?label?
-* Skok na navesti s podporou navratu
-* Ulozi inkrementovanou aktualni pozici z interniho citace instrukci do zasobniku volani
-* a provede skok na zadan� navest�(pripadnou pripravu ramce musi zajistit jine instrukce).
-*/
-void CALL(Node* c_label)
-{
-	return;
-}
-
-/*
-* RETURN 
-* Navrat na pozici ulozenou instrukci CALL
-* Vyjme pozici ze zasobniku volani a skoci na tuto pozici nastavenim intern�ho citace
-* instrukci(uklid lokalnich ramcu musi zajistit jine instrukce).Provedeni instrukce pri
-* prazdnem zasobniku volani vede na chybu 56.
-*/
-void RETURN()
-{
-	return;
-}
-
-/*
-* PUSHS ?symb?
-* Vlou hodnotu na vrchol datoveho zasobniku
-* Ulozi hodnotu ?symb? na datovy zasobnik.
-*/
-void PUSHS(Node* c_symb)
-{
-	return;
-}
-
-/* POPS ?var?
-* Vyjmi hodnotu z vrcholu datoveho zasobniku
-* Neni-li z�sobnik prazdny, vyjme z nej hodnotu a ulozi ji do promenne?var?,
-* jinak dojde k chybe 56.
-*/
-void POPS(Node* c_var)
-{
-	return;
-}
-
-/*
-* CLEARS
-* Vymazani obsahu celeho datov�ho zasobniku
-* Pomocna instrukce, ktera smaze cely obsah datov�ho zasobniku,
-* aby neobsahoval zapomenute hodnoty z predchozich vypoctu.
-*/
-void CLEARS()
-{
-	return;
-}
-
 void ADD(Node* c_symb)
 {
 	if (c_symb == NULL)
@@ -718,18 +660,6 @@ void MUL(Node* c_symb)
 	PrintCode("\n");
 }
 
-/*
-* DIV ?var? ?symb1? ?symb2?
-* Delen� dvou desetinn�ch hodnot. Podel� hodnotu ze ?symb1?
-* druhou hodnotou ze ?symb2? (oba mus� b�t typu float)
-* a v�sledek prirad� do promenn� ?var? (t� typu float).
-* Delen� nulou zpusob� chybu 57.
-*/
-void DIV(Node* c_symb)
-{
-	return;
-}
-
 void IDIV(Node* c_symb)
 {
 	if (c_symb == NULL)
@@ -737,128 +667,6 @@ void IDIV(Node* c_symb)
 	PrintCode("IDIV GF@expressionSum GF@expressionSum ");
 	PrintSymbol(c_symb);
 	PrintCode("\n");
-}
-
-/*
-* ADDS / SUBS / MULS / DIVS / IDIVS
-* Z�sobn�kov� verze instrukc� ADD, SUB, MUL, DIV a IDIV
-*/
-/* might not need */
-
-/*
-* LT / GT / EQ ?var? ?symb1? ?symb2? 
-* Relacn� oper�tory men��, vet��, rovno
-* Instrukce vyhodnot� rela?n� oper�tor mezi ?symb1? a ?symb2?
-* (stejn�ho typu; int, bool, float nebo string)
-* a do booleovsk� promenn� ?var? zap�e false pri neplatnosti 
-* nebo true v pr�pade platnosti odpov�daj�c� relace.
-* retezce jsou porovn�v�ny lexikograficky a false je men�� ne� true.
-* Pro v�pocet neostr�ch nerovnost� lze pou��t AND / OR / NOT.
-* S operandem typu nil(druh� operand je libovoln�ho typu)
-* lze porovn�vat pouze instrukc� EQ, jinak chyba 53.
-*/
-void COMPARE(char* c_option, Node* c_var, Node* c_symb1, Node* c_symb2)
-{
-	return;
-}
-
-/*
-* LTS / GTS / EQS
-* Zasobnikov� verze instrukci LT / GT / EQ
-*/
-/* might not need */
-
-/*
-* AND / OR / NOT ?var? ?symb1? ?symb2?
-* Z�kladn� booleovsk� oper�tory
-* Aplikuje konjunkci(logick� A) / disjunkci(logick� NEBO) 
-* na operandy typu bool ?symb1? a ?symb2?
-* nebo negaci na ?symb1? (NOT m� pouze 2 operandy)
-* a v�sledek typu bool zap�e do ?var?.
-*/
-void LOGIC(char* c_option,Node* c_var, Node* c_symb1, Node* c_symb2)
-{
-	return;
-}
-
-/*
-* ANDS / ORS / NOTS
-* Z�sobn�kov� verze instrukc� AND, OR a NOT
-*/
-/* might not need */
-
-/* 
-* INT2FLOAT ?var? ?symb? Prevod celoc�seln� hodnoty na desetinnou
-* Prevede celoc�selnou hodnotu ?symb? na desetinn� c�slo
-* a ulo�� je do ?var?.
-*/
-void INT2FLOAT(Node* c_var, Node* c_symb)
-{
-	return;
-}
-
-/*
-* FLOAT2INT ?var? ?symb?
-* Prevod desetinn� hodnoty na celoc�selnou(oseknut�)
-* Prevede desetinnou hodnotu ?symb?
-* na celoc�selnou oseknut�m desetinn� c�sti a ulo��	ji do ?var?.
-*/
-void FLOAT2INT(Node* c_var, Node* c_symb)
-{
-	return;
-}
-
-/*
-* INT2CHAR ?var? ?symb?
-* Prevod cel�ho c�sla na znak
-* c�seln� hodnota ?symb? je dle ASCII prevedena na znak, kter� tvor� 
-* jednoznakov� retezec prirazen� do ?var?.
-* Je-li ?symb? mimo interval[0; 255], dojde k chyb? 58.
-*/
-void INT2CHAR(Node* c_var, Node* c_symb)
-{
-	return;
-}
-
-/*
-* STRI2INT ?var? ?symb1? ?symb2?
-* Ordin�ln� hodnota znaku.
-* Do ?var? ulo�� ordin�ln� hodnotu znaku(dle ASCII)
-* v retezci ?symb1? na pozici ?symb2? (indexov�no od nuly).
-* Indexace mimo dan� ?et?zec vede na chybu 58.
-*/
-void STRI2INT(Node* c_var, Node* c_symb1, Node* c_symb2)
-{
-	return;
-}
-
-/*
-* INT2FLOATS / FLOAT2INTS /	INT2CHARS / STRI2INTS
-* Z�sobn�kov� verze konverzn�ch instrukc�
-*/
-/* might not need */
-
-/*
-* CONCAT ?var? ?symb1? ?symb2?
-* Konkatenace dvou retezcu.
-* Do promenn� ?var? ulo�� retezec vznikl�
-* konkatenac� dvou retezcov�ch operandu	?symb1? a ?symb2?
-* (jin� typy nejsou povoleny).
-*/
-void CONCAT(Node* c_var, Node* c_symb1, Node* c_symb2)
-{
-	return;
-}
-
-/*
-* STRLEN ?var? ?symb?
-* Zjisti d�lku retezce.
-* Zjist� d�lku retezce v ?symb? a d�lka je ulo�ena jako
-* cel� c�slo do ?var?.
-*/
-void STRLEN(Node* c_var, Node* c_symb)
-{
-	return;
 }
 
 /*
@@ -888,117 +696,16 @@ void SETCHAR(Node* c_var, Node* c_symb1, Node* c_symb2)
 {
 	return;
 }
-
-/*
-* TYPE ?var? ?symb?
-* Zjisti typ dan�ho symbolu.
-* Dynamicky zjist� typ symbolu ?symb? a do ?var?
-* zap�e retezec znac�c� tento typ
-* (int, bool, float, string nebo nil).
-* Je-li ?symb? neinicializovan� promenn�, oznac� jej� typ
-* pr�zdn�m retezcem.
-*/
-void TYPE(Node* c_var, Node* c_symb)
-{
-	return;
-}
-
-/*
-* LABEL ?label?
-* Definice n�ve�t�.
-* Speci�ln� instrukce oznacuj�c� pomoc� n�ve�t� ?label?
-* dule�itou pozici v k�du jako potenci�ln� c�l libovoln� skokov� instrukce.
-* Pokus o redefinici existuj�c�ho n�ve�t� je chybou 52.
-*/
-void LABEL(Node* c_label, char* type)
-{
-	return;
-}
-
-/*
-* JUMP ?label? 
-* Nepodm�nen� skok na n�ve�t�.
-* Provede nepodm�nen� skok na zadan� n�ve�t� ?label?.
-*/
-void JUMP(Node* c_label)
-{
-	return;
-}
-
-/*
-* JUMPIFEQ ?label? ?symb1? ?symb2?
-* Podm�nen� skok na n�ve�t� pri rovnosti
-* Pokud jsou ?symb1? a ?symb2? stejn�ho typu nebo 
-* je nekter� operand nil(jinak chyba 53)
-* a z�roven se jejich hodnoty rovnaj�, tak provede skok 
-* na n�ve�t� ?label?.
-*/
-void JUMPIFEQ(Node* c_label, Node* c_symb1, Node* c_symb2)
-{
-	return;
-}
-
-/*
-* JUMPIFNEQ ?label? ?symb1? ?symb2?
-* Podm�nen� skok na n�ve�t� pri nerovnosti.
-* Jsou-li ?symb1? a ?symb2? stejn�ho typu
-* nebo je n?kter� operand nil(jinak chyba 53),
-* ale ruzn� hodnoty, tak provede skok na n�ve�t� ?label?.
-*/
-void JUMPIFNEQ(Node* c_label, Node* c_symb1, Node* c_symb2)
-{
-	return;
-}
-
-/*
-* JUMPIFEQS / JUMPIFNEQS ?label?
-* Z�sobn�kov� verze JUMPIFEQ, JUMPIFNEQ
-* Z�sobn�kov� skokov� instrukce
-* maj� i jeden operand mimo datov� z�sobn�k, a to n�ve�t� ?label?,
-* na kter� se pr�padne provede skok.
-*/
-/* might not need */
-
-/*
-* EXIT ?symb?
-* Ukoncen� interpretace s n�vratov�m k�dem
-* Ukonc� vykon�v�n� programu a ukonc� interpret
-* s n�vratov�m k�dem ?symb?, kde ?symb? je cel� c�slo
-* v intervalu 0 a� 49 (vcetne).
-* Nevalidn� celoc�seln� hodnota ?symb? vede na chybu 57.
-*/
-void EXIT(Node* c_symb)
-{
-	return;
-}
-
-/*
-* BREAK
-* V�pis stavu interpretu na stderr.
-* Na standardn� chybov� v�stup(stderr) vyp�e stav interpretu
-* v danou chv�li(tj.behem vykon�v�n� t�to instrukce). 
-* Stav se mimo jin� skl�d� z pozice v k�du, v�pisu glob�ln�ho,
-* aktu�ln�ho lok�ln�ho a do?asn�ho r�mce a poctu ji�
-* vykonan�ch instrukc�.
-*/
-void BREAK()
-{
-	return;
-}
-
-/*
-* DPRINT ?symb?
-* V�pis hodnoty na stderr.
-* Vyp�e zadanou hodnotu ?symb? na standardn� chybov� v�stup(stderr).
-* V�pisy touto instrukc� bude mo�n� vypnout pomoc� volby interpretu.
-*/
-void DPRINT(Node* c_symb)
-{
-	return;
-}
-
 /* func readString() -> String? */
+void INBUILT_READSTRING(Node* c_node)
+{
+
+}
 /* func readInt() -> Int? */
+void INBUILT_READINT(Node* c_node)
+{
+
+}
 /* func write(term1 , term2 , ..., termx) */
 void INBUILT_WRITE(Node* c_node)
 {
@@ -1010,8 +717,111 @@ void INBUILT_WRITE(Node* c_node)
 	}
 }
 /* func Int2Double(_ term : Int) -> Double */
+void INBUILT_INT2DOUBLE(Node* c_node)
+{
+	if (c_node->numChildren == 0)
+		return;
+
+	PrintCode("INT2FLOAT ");
+	PrintCode("GF@expressionSum ");
+	PrintSymbol(c_node->children[0]);
+	PrintCode("\n");
+
+	if (strcmp(c_node->parent->type, "assign") == 0)
+	{
+		PrintCode("MOVE ");
+		PrintSymbol(c_node->parent->children[0]);
+		PrintCode(" ");
+		PrintCode("GF@expressionSum");
+		PrintCode("\n");
+	}
+}
 /* func Double2Int(_ term : Double) */
+void INBUILT_DOUBLE2INT(Node* c_node)
+{
+	if (c_node->numChildren == 0)
+		return;
+
+	PrintCode("FLOAT2INT ");
+	PrintCode("GF@expressionSum ");
+	PrintSymbol(c_node->children[0]);
+	PrintCode("\n");
+
+	if (strcmp(c_node->parent->type, "assign") == 0)
+	{
+		PrintCode("MOVE ");
+		PrintSymbol(c_node->parent->children[0]);
+		PrintCode(" ");
+		PrintCode("GF@expressionSum");
+		PrintCode("\n");
+	}
+}
 /* func length(_ s : String) -> Int */
+void INBUILT_LENGTH(Node* c_node)
+{
+	if (c_node->numChildren == 0)
+		return;
+
+	PrintCode("STRLEN ");
+	PrintCode("GF@expressionSum ");
+	PrintSymbol(c_node->children[0]);
+	PrintCode("\n");
+
+	if (strcmp(c_node->parent->type, "assign") == 0)
+	{
+		PrintCode("MOVE ");
+		PrintSymbol(c_node->parent->children[0]);
+		PrintCode(" ");
+		PrintCode("GF@expressionSum");
+		PrintCode("\n");
+	}
+}
 /* func substring(of s : String, startingAt i : Int, endingBefore j :Int) -> String? */
+void INBUILT_SUBSTRING(Node* c_node)
+{
+	if (c_node->numChildren == 0)
+		return;
+
+}
 /* func ord(_ c : String) -> Int */
+void INBUILT_ORD(Node* c_node)
+{
+	if (c_node->numChildren == 0)
+		return;
+
+	PrintCode("STRI2INT ");
+	PrintCode("GF@expressionSum ");
+	PrintSymbol(c_node->children[0]);
+	PrintCode(" INT@0");
+	PrintCode("\n");
+
+	if (strcmp(c_node->parent->type, "assign") == 0)
+	{
+		PrintCode("MOVE ");
+		PrintSymbol(c_node->parent->children[0]);
+		PrintCode(" ");
+		PrintCode("GF@expressionSum");
+		PrintCode("\n");
+	}
+}
 /* func chr(_ i : Int) -> String */
+void INBUILT_CHR(Node* c_node)
+{
+
+	if (c_node->numChildren == 0)
+		return;
+
+	PrintCode("INT2CHAR ");
+	PrintCode("GF@expressionSum ");
+	PrintSymbol(c_node->children[0]);
+	PrintCode("\n");
+
+	if (strcmp(c_node->parent->type, "assign") == 0)
+	{
+		PrintCode("MOVE ");
+		PrintSymbol(c_node->parent->children[0]);
+		PrintCode(" ");
+		PrintCode("GF@expressionSum");
+		PrintCode("\n");
+	}
+}
